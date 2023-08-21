59d5c41a48236275b58ec8f1bb8b7e4b65ed3bd9
alter412 <139802395+alter412@users.noreply.github.com>
2023-08-21 13:24:24 +0530

    Remove some code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,27 +2,6 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount ba = new BankAccount("Test",30000,5000);
-        try {
-            String ac =ba.generateAccountNumber(9,9);
-            System.out.println(ac);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-
-
-        CurrentAccount ca = null;
-        try {
-            ca = new CurrentAccount("Test2",200000,"AAABB");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            ca.validateLicenseId();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
 
     }
 }
